5231942179f14ef54909d40a91c62bd714513250
Elena Piaggio <elenapiaggio@gmail.com>
2021-05-02 19:38:41 +0200

    ADD first test

--- src/test/java/rover/RoverShould.java
+++ src/test/java/rover/RoverShould.java
@@ -1,2 +1,27 @@
-package rover;public class RoverShould {
+package rover;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+
+public class RoverShould {
+
+  private Rover rover;
+
+  @Before
+  public void initialise(){
+    rover = new Rover();
+  }
+
+  @Test
+  public void rotate_right(){
+
+    assertThat(rover.execute("R"), is("0:0:E"));
+
+  }
 }
